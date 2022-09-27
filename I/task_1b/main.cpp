bb5a44442a581b72f1ff917bde9ef9e33a7e80a6
AlexDev404 <alexdev404@users.noreply.github.com>
2022-09-26 22:58:49 -0600

    Finish task_1b

--- I/task_1b/main.cpp
+++ I/task_1b/main.cpp
@@ -2,15 +2,34 @@
 using namespace std;
 
 void greeting();
+int sumNumbers(int num1, int num2);
+double getAreaOfSquare(double side);
 
 int main()
 {
+    // A
+    cout << "A\n";
     greeting();
+    // B
+    cout << "B\n";
+    cout << sumNumbers(); << endl;
+    // C
+    cout << "C\n";
+    cout << getAreaOfSquare(2, 2) << endl;
+    // D
+    cout << "D\n";
+    cout << getLastDigit(14) << endl;
+    // E
+    cout << "E\n";
+    cout << getAreaOfRect(2, 2) << endl;
+    // F
+    cout << "F\n";
+    cout << sumNumbers(2, 2) << endl;
 }
 
 // Av+ F
 /**
- * @brief Returns a string
+ * @brief Prints out a string to the console
  * @return void
  */
 void greeting()
@@ -25,41 +44,44 @@ void greeting()
  */
 int sumNumbers(int num1, int num2)
 {
-    if(num1 && num2){
+    // Checks if this function was called with arguments
+    if (num1 && num2)
+    {
         return num1 + num2;
     }
 
-
+// Otherwise we do this 
     int userinput = 0;
     int result = 0;
-    for (int i = 0; i < 2; i++ && userinput = userinput + result;)
+
+    for (int i = 0; i < 2; i++ &&userinput = userinput + result;)
     {
         cout << "Enter 2 Integers: ";
         cin >> userinput;
     }
     return result;
-    
 }
 
 // C
 /**
  * @brief Gets the area of a square
  * @param side Length of a side of square
- * @return Area of dquare
+ * @return Area of square
  */
-double getAreaOfSquare(double side){
-    // Calculate and output side of square
-    return sideOfSquare
+double getAreaOfSquare(double side)
+{
+    // Calculate and return area of square
+    return side * side;
 }
-
 
 // D
 /**
  * @brief Gets last digit of integer
  * @param age Person's age
- * @return 
+ * @return
  */
-int getLastDigit(int age){
+int getLastDigit(int age)
+{
     // https://stackoverflow.com/a/3001595/10976415
     return age % 10;
 }
@@ -71,6 +93,11 @@ int getLastDigit(int age){
  * @param width Rect's width
  * @return Area of rectangle
  */
-double getAreaOfRect(double length, double width){
-
+double getAreaOfRect(double length, double width)
+{
+    // Calcalate and return area of rect
+    return length * width;
 }
+
+
+// Question "F" was merged into question "A"
