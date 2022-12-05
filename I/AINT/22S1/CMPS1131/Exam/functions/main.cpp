075e57221d59392d48b955f6219cf5169931e70d
AlexDev404 <alexdev404@users.noreply.github.com>
2022-12-04 22:58:30 -0600

    Balls

--- I/AINT/22S1/CMPS1131/Exam/functions/main.cpp
+++ I/AINT/22S1/CMPS1131/Exam/functions/main.cpp
@@ -5,21 +5,5 @@ void sus();
 
 int main()
 {
-    sus();
     return 0;
-}
-
-// Typecasting
-void sus(string a, double x) // string, double
-{
-    // loop 20 times
-    int xd = 20;
-    for (int i = 1; i <= xd; i++)
-    {
-        cout << a << " is Among us " << x; // add to string here
-    }
-    while()
-    {
-
-    }
 }
